dcce03baf57d04f28c367530979f405d087e3494
Gurparas-Singh <78079839+Gurparas-Singh@users.noreply.github.com>
2021-07-07 19:39:04 +0530

    Add files via upload

---
+++ blockGreen.js
@@ -0,0 +1,14 @@
+class blockGreen{
+    constructor(x,y){
+        this.body = Bodies.rectangle(x,y,20,30,{isStatic:false,'friction':1.0,'density':5.0});
+        World.add(world,this.body);
+    }
+    display(){
+        var pos = this.body.position;
+        push();
+        rectMode(CENTER);
+        fill("green");
+        rect(pos.x,pos.y,20,30);
+        pop();
+    }
+}
